e4a29eb66a1c6653e5e883a6ee32a3b6bbb26b20
Joel Francisco Petenatti <jfpetenatti@gmail.com>
2022-08-22 19:54:43 -0300

    Se agregan geters y seters correspondientes a la clase tecnico

--- Guia0/Tecnico.java
+++ Guia0/Tecnico.java
@@ -14,7 +14,29 @@ public class Tecnico extends Empleado {
         this.adicional_titulo = adicional_titulo;
     }
 
-    
+    protected String getTitulo() {
+        return titulo;
+    }
 
-    
+    protected void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    protected int getAnio_titulo() {
+        return anio_titulo;
+    }
+
+    protected void setAnio_titulo(int anio_titulo) {
+        this.anio_titulo = anio_titulo;
+    }
+
+    protected int getAdicional_titulo() {
+        return adicional_titulo;
+    }
+
+    protected void setAdicional_titulo(int adicional_titulo) {
+        this.adicional_titulo = adicional_titulo;
+    }
 }
+
+
