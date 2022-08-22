bacc0e20d0b5e47b5eb9b1bbd955538805030eef
Joel Francisco Petenatti <jfpetenatti@gmail.com>
2022-08-22 19:52:42 -0300

    Se agrega constructor de la clase tecnico

--- Guia0/Tecnico.java
+++ Guia0/Tecnico.java
@@ -7,6 +7,13 @@ public class Tecnico extends Empleado {
     protected int anio_titulo;
     protected int adicional_titulo;
 
+    public Tecnico(String titulo, int anio_titulo, int adicional_titulo, String legajo) {
+        super(legajo);
+        this.titulo = titulo;
+        this.anio_titulo = anio_titulo;
+        this.adicional_titulo = adicional_titulo;
+    }
+
     
 
     
