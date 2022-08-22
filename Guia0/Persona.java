6efac96ee691ceba1c221da95d34c345618ea251
matias cornejo <maticornejo99@gmail.com>
2022-08-22 18:06:38 -0300

    Le agregue atributos y el constructor a la clase Persona

--- Guia0/Persona.java
+++ Guia0/Persona.java
@@ -1,5 +1,13 @@
 package Guia0;
 
 public class Persona {
-    
+    protected String nombre;
+    protected String apellido;
+    protected String dni;
+    protected String direccion;
+    protected String fecha_Nacimiento;
+    protected String sexo;
+
+    public Persona (){ 
+    }
 }
