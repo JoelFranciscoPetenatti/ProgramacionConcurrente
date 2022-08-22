47f65e7c46a9a56c643dea560a3bdba59205443b
Joel Francisco Petenatti <jfpetenatti@gmail.com>
2022-08-22 20:06:14 -0300

    Agregamos atributos y posible constructor correspondiente a la clase administrativo

--- Guia0/Administrativo.java
+++ Guia0/Administrativo.java
@@ -2,5 +2,13 @@ package Guia0;
 
 public class Administrativo extends Empleado {
     
+    protected String categoria;
+    protected int adicional_categoria;
+    
+    /*public Administrativo(String nombre, String apellido, String dni) {
+        super(nombre, apellido, dni);
+    }*/
+
+
     
 }
