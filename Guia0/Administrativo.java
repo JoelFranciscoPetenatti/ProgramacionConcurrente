860535715bc8cd090f825ffa60c3abb1233999dd
Joel Francisco Petenatti <jfpetenatti@gmail.com>
2022-08-22 20:11:23 -0300

    Se agrega a la clase administrativo el constructor, ademas de geters and seters

--- Guia0/Administrativo.java
+++ Guia0/Administrativo.java
@@ -9,6 +9,28 @@ public class Administrativo extends Empleado {
         super(nombre, apellido, dni);
     }*/
 
+    public Administrativo(String categoria, int adicional_categoria, String legajo) {
+        super(legajo);
+        this.categoria = categoria;
+        this.adicional_categoria = adicional_categoria;
+    }
+    
+    protected String getCategoria() {
+        return categoria;
+    }
 
+    protected void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    protected int getAdicional_categoria() {
+        return adicional_categoria;
+    }
+
+    protected void setAdicional_categoria(int adicional_categoria) {
+        this.adicional_categoria = adicional_categoria;
+    }
     
 }
+    
+
