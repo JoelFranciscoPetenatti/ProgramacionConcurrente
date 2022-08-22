40ac445be59dc1c98bd1e45c657d74a7ccb55a36
matias cornejo <maticornejo99@gmail.com>
2022-08-22 19:12:21 -0300

    Agregue el metodo que lista los empleados con antiguedad mayor a un numero dado

--- Guia0/Empresa.java
+++ Guia0/Empresa.java
@@ -3,15 +3,29 @@ package Guia0;
 import java.util.*;
 
 public class Empresa {
-    
+
     public String nombre_Empresa;
-    public List <Empleado> empleados = new LinkedList <Empleado> ();
-    
-    public Empresa (String nombre){
+    public List<Empleado> empleados = new LinkedList<Empleado>();
+
+    public Empresa(String nombre) {
         nombre_Empresa = nombre;
     }
-    
-    public void agregarEmpleado (Empleado emp){
+
+    public void agregarEmpleado(Empleado emp) {
         empleados.add(emp);
+    }
+
+    /*
+     * Metodo que lista los empleados, de la empresa, que su antiguedad es mayor al
+     * numero de años que deseemos comparar
+     */
+    public List<Empleado> empleados_antiguedad_mayor() {
+        List<Empleado> empleados_cond = new LinkedList<Empleado>();
+        for (int i = 0; i < this.empleados.size(); i++) {
+            if (empleados.get(i).getAntiguedad() > 10) {
+                empleados_cond.add(empleados.get(i));
+            }
+        }
+        return empleados_cond;
     }
 }
