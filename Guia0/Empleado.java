664aab3184c3327b6bbef916a8ae6a51cf590aa6
matias cornejo <maticornejo99@gmail.com>
2022-08-22 19:03:24 -0300

    Agregue atributos y constructor de la clase Empleado

--- Guia0/Empleado.java
+++ Guia0/Empleado.java
@@ -1,5 +1,14 @@
 package Guia0;
 
-public class Empleado {
+public class Empleado extends Persona {
+    protected String legajo;
+    protected int antiguedad;
+    protected int porc_antiguedad;
+    protected String funcion;
+    protected int salario;
+
+    public Empleado(String legajo){       
+        this.legajo=legajo;
+    }
     
 }
