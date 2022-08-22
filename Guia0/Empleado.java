2eadd49d59af05a33a7bb713c6236567de72af88
matias cornejo <maticornejo99@gmail.com>
2022-08-22 19:04:41 -0300

    Agregue los getters y setters de la clase Empleado

--- Guia0/Empleado.java
+++ Guia0/Empleado.java
@@ -10,5 +10,44 @@ public class Empleado extends Persona {
     public Empleado(String legajo){       
         this.legajo=legajo;
     }
-    
+
+    public String getLegajo() {
+        return legajo;
+    }
+
+    protected void setLegajo(String legajo) {
+        this.legajo = legajo;
+    }
+
+    public int getAntiguedad() {
+        return antiguedad;
+    }
+
+    public void setAntiguedad(int antiguedad) {
+        this.antiguedad = antiguedad;
+    }
+
+    protected int getPorc_antiguedad() {
+        return porc_antiguedad;
+    }
+
+    protected void setPorc_antiguedad(int porc_antiguedad) {
+        this.porc_antiguedad = porc_antiguedad;
+    }
+
+    protected String getFuncion() {
+        return funcion;
+    }
+
+    protected void setFuncion(String funcion) {
+        this.funcion = funcion;
+    }
+
+    protected int getSalario() {
+        return salario;
+    }
+
+    protected void setSalario(int salario) {
+        this.salario = salario;
+    }
 }
